f020305574450270e97e4c2a881ce61a1a5579a9
motorcykle <mohamedabdulridha1@gmail.com>
2021-07-14 11:58:42 +0200

    doc page name change + fix

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const doc = () => {
+const Doc = () => {
   return (
     <div>
-      
+      <h1>Doc</h1>
     </div>
   );
 }
 
-export default doc;
+export default Doc;
